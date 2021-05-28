74f7953ff6d637be617b094014b6612b5ed6dc60
Gregor Aisch <gka@users.noreply.github.com>
2021-05-28 08:06:26 +0200

    update

--- 2021-05-27-weather-forecast/weather-forecast-table.R
+++ 2021-05-27-weather-forecast/weather-forecast-table.R
@@ -2,12 +2,15 @@
 # library('devtools')
 # devtools::install_github("retostauffer/Rmosmix")
 # library("mosmix")
+library(readr)
+library(dplyr)
+library(weathermetrics)
+library(mosmix)
 
-needs(dplyr, weathermetrics, mosmix, readr)
+station_id <- 'C720' # Berlin-Tempelhof
 
-station_id <- '10147' # Berlin-Tempelhof
-
-url <- paste0('https://opendata.dwd.de/weather/local_forecasts/mos/MOSMIX_L/single_stations/', station_id, '/kml/MOSMIX_L_LATEST_',station_id,'.kmz')
+# url <- paste0('https://opendata.dwd.de/weather/local_forecasts/mos/MOSMIX_L/single_stations/', station_id, '/kml/MOSMIX_L_LATEST_',station_id,'.kmz')
+url <- 'https://opendata.dwd.de/weather/local_forecasts/mos/MOSMIX_S/all_stations/kml/MOSMIX_S_LATEST_240.kmz'
 
 kmz   <- tempfile("mosmix_demo_", fileext = ".kmz")
 check <- download.file(url, kmz)
@@ -19,22 +22,43 @@ doc <- XML::xmlParse(kml)
 datetime <- get_datetime(doc)
 meta     <- get_meta_info(doc)
 
+stations <- read_fwf('https://www.dwd.de/EN/ourservices/met_application_mosmix/mosmix_stations.cfg;jsessionid=6A47F126BB1F3B5383F07B7CC03595E9.live31092?view=nasPublication&nn=495490',
+                      col_positions = fwf_widths(c(5,6,6,5,21,7,8,6,7,5),
+                                                 col_names = c('clu','CofX','id','ICAO', 'name','lat','lon','elev','Hmod-H', 'type')), skip = 4)
 
-fcst <- get_forecasts(station_id, doc, datetime, meta, as.zoo = FALSE)
+good.stations <- stations %>% filter(!is.na(as.numeric(id)) & type=='LAND')
 
-by.day <- fcst %>%
-  transmute(date=as.Date(datetime),
-            temp=kelvin.to.celsius(TTT),
-            sun=SunD1/60,
-            rain=RR1c) %>%
-  group_by(date) %>%
-  filter(n()==24) %>% 
-  summarise(temp.min=round(min(temp)), temp.max=round(max(temp)), sunshine.hours=round(sum(sun)/60,0), rain.mm=sum(rain))
+final_stations <- c()
 
-by.day %>% 
-  transmute(day=format(date, '%a.'),
-         sun=paste0('![](https://vis4.net/i/sun', as.numeric(cut(sunshine.hours, breaks=c(-1,0.5,4,9,24)))-1,'.png) ', sunshine.hours, 'h'),
-         rain=paste0('![](https://vis4.net/i/rain',as.numeric(cut(rain.mm, breaks=c(-1,0,2,6,100)))-1,'.png) ', round(rain.mm), 'mm'),
-         temp=paste0(temp.max,'<span style="color:#888"> / ', temp.min,'°C</span>')) %>% 
-  write_csv('forecast.csv')
+dir.create('out', showWarnings=F)
+dir.create('out/stations', showWarnings = F)
+
+for (station_id in good.stations$id) {
+  fcst <- get_forecasts(station_id, doc, datetime, meta, as.zoo = FALSE)
+  if (!is.null(fcst) & !is.null(fcst$SunD1)) {
+    by.day <- fcst %>%
+      transmute(date=as.Date(datetime),
+                temp=kelvin.to.celsius(TTT),
+                sun=SunD1/60,
+                rain=RR1c) %>%
+      group_by(date) %>%
+      filter(n()==24) %>% 
+      summarise(temp.min=round(min(temp)), temp.max=round(max(temp)), sunshine.hours=round(sum(sun)/60,0), rain.mm=sum(rain))
+    
+    by.day %>% 
+      transmute(day=format(date, '%a.'),
+                sun=paste0('![](https://vis4.net/i/sun', as.numeric(cut(sunshine.hours, breaks=c(-1,0.5,4,9,24)))-1,'.png) ', sunshine.hours, 'h'),
+                rain=paste0('![](https://vis4.net/i/rain',as.numeric(cut(rain.mm, breaks=c(-1,0,2,6,100)))-1,'.png) ', round(rain.mm), 'mm'),
+                temp=paste0(temp.max,'<span style="color:#888"> / ', temp.min,'°C</span>')) %>% 
+      write_csv(paste0('out/stations/forecast-', station_id, '.csv'))
+    
+    final_stations <- append(final_stations, station_id)
+    print(station_id)
+  }
+}
+
+good.stations %>% filter(id %in% final_stations) %>% write_csv('out/stations.csv')
+
+
+
 
